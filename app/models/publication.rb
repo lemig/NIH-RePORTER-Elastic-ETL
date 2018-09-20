65a4a731a36570b8c87384dc11224ef21d656982
Miguel Cabero <lemig@hotmail.com>
2018-09-20 17:16:04 +0200

    Add missing author_names method to Publication

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -1,3 +1,8 @@
 class Publication < ActiveRecord::Base
   belongs_to :exporter_file
+  has_and_belongs_to_many :authors, class_name: 'Person'
+
+  def author_names
+    author_list.to_s.gsub('<![CDATA[','').gsub(']]>','').split(';').map(&:strip)
+  end
 end
